2c752b3592ce63884a530b721a7878caf01154d1
Hary <1185252665@qq.com>
2021-02-10 15:30:39 +0800

    主分支第一次提交

---
+++ src/main/java/com/herui/LearninggitApplication.java
@@ -0,0 +1,13 @@
+package com.herui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninggitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearninggitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herui/LearninggitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearninggitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
